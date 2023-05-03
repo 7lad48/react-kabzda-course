44bd753591ca65d0d1d1361e2925dcc2b10a4aa2
7lad48 <2335213@gmail.com>
2023-05-03 15:08:57 +0300

    conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,31 @@ export default function App() {
     console.log('App is rendering')
     return (
         <div className="App">
-            <Title title="This is app component"/>
-            <Title title="my asdjasd"/>
-            Article 1
-            <Rating value={3} />
+            {/*<Title title="This is app component"/>*/}
+            {/*<Title title="my asdjasd"/>*/}
+            {/*Article 1*/}
+            {/*<Rating value={3} />*/}
             <Accordion titleValue="Title num one" collapsed={false} />
-            <Accordion titleValue="Title number Two" collapsed={true} />
-            article 2
-            <Rating value={0} />
-            <Rating value={1} />
-            <Rating value={2} />
-            <Rating value={3} />
-            <Rating value={4} />
-            <Rating value={5} />
+            <Accordion titleValue="Title number Two" collapsed={false} />
+            {/*article 2*/}
+            {/*<Rating value={0} />*/}
+            {/*<Rating value={1} />*/}
+            {/*<Rating value={2} />*/}
+            {/*<Rating value={3} />*/}
+            {/*<Rating value={4} />*/}
+            {/*<Rating value={5} />*/}
         </div>
     );
 }
-type TitlePropsType = {
-    title: string
-}
-function Title(props: TitlePropsType) {
-    console.log('Title is renderinsg')
-    return (
-        <h2>
-            {props.title}
-        </h2>
-    );
-}
+// type TitlePropsType = {
+//     title: string
+// }
+// function Title(props: TitlePropsType) {
+//     console.log('Title is renderinsg')
+//     return (
+//         <h2>
+//             {props.title}
+//         </h2>
+//     );
+//}
 
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -6,18 +6,12 @@ type AccordionPropsType = {
 }
 export default function Accordion(props: AccordionPropsType) {
     console.log('Accordion is rendering')
-    if(props.collapsed){
-        return <div>
-            <AccordionTitle title={props.titleValue}/>
-    </div>
-    } else {
         return (
             <div>
                 <AccordionTitle title={props.titleValue}/>
-                <AccordionBody/>
+                {!props.collapsed && <AccordionBody/>}
             </div>
         );
-    }
 }
 
 type AccordionTitlePropsType = {
